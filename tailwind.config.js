804ad88b3a4568088b2a3d3a75bb530932d90a74
yashpardeshi <87255877+yashpardeshi@users.noreply.github.com>
2025-08-09 18:36:38 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./{app,components,libs,pages,hooks}/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
